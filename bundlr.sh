f09a116b1003f44e2277a661e278fd70fc19a642
MrN1x0n <95374645+MrN1x0n@users.noreply.github.com>
2022-07-23 01:26:39 +0300

    Update bundlr.sh

--- bundlr.sh
+++ bundlr.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-exists()
-{
-  command -v "$1" >/dev/null 2>&1
-}
-if exists curl; then
-echo ''
-else
-  sudo apt update && sudo apt install curl -y < "/dev/null"
-fi
-bash_profile=$HOME/.bash_profile
-if [ -f "$bash_profile" ]; then
-    . $HOME/.bash_profile
-fi
-
-
 while true
 do
 
